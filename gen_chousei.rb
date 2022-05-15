ba206a3f8b1faba74388ed3d192b7a64cbe84556
eunaet <srtnue@gmail.com>
2022-05-15 13:38:32 +0900

    常に最新の年になるように修正

--- gen_chousei.rb
+++ gen_chousei.rb
@@ -7,7 +7,7 @@ days_in_month = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 wd = [/Mon/, /Tue/, /Wed/, /Thu/, /Fri/, /Sat/, /Sun/]
 wd_jp = %w[月 火 水 木 金 土 日]
 str = ''
-time = Time.new(2021, month, 1, 0, 0, 0, '+09:00')
+time = Time.new(Time.now.year, month, 1, 0, 0, 0, '+09:00')
 
 (1..days_in_month[month]).each do |_day|
   str += "#{time.strftime('%-m/%-d (%a)')} 21:00 ~ \n"
