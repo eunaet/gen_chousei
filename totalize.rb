9587327a34ac1847b562f4899df1a8f4f448529e
eunaet <srtnue@gmail.com>
2022-08-23 00:47:41 +0900

    回答者数が不足している場合に結果を表示しない

--- totalize.rb
+++ totalize.rb
@@ -2,6 +2,9 @@ require 'capybara'
 require 'webdrivers/chromedriver'
 require_relative 'send_message_to_discord'
 
+MINIMUM_ANSWER = 6
+NOT_ENOUGH = "回答が3人未満です"
+
 Capybara.threadsafe = true
 
 session = Capybara::Session.new(:selenium_chrome_headless) do |config|
@@ -9,27 +12,39 @@ session = Capybara::Session.new(:selenium_chrome_headless) do |config|
   config.app_host = 'https://chouseisan.com'
 end
 
-results = []
-session.visit ARGV[0]
-session.within(:id, 'tableArea') do
-  session.all('tr').each do |tr|
-    tds = tr.all('td').map(&:text)
-    next unless %r{\d/\d}.match?(tds[0])
-    next unless tds[3].to_i.zero?
+# td[0]:日程
+# td[1..3]:○△✕
+# td[4..]:回答
+def visit_page(session, url)
+  results = []
+  session.visit(url)
+  session.within(:id, 'tableArea') do
+    session.all('tr').each do |tr|
+      tds = tr.all('td').map(&:text)
+      break unless tds.length > MINIMUM_ANSWER
+      next unless %r{\d/\d}.match?(tds[0])
+      next unless tds[3].to_i.zero?
 
-    total = tds[1].to_i + tds[2].to_i
-    results << { day: tds[0], total: total } unless total.zero?
+      total = tds[1].to_i + tds[2].to_i
+      results << { day: tds[0], total: total } unless total.zero?
+    end
+  end
+  results
+end
+
+def result_text(results)
+  return NOT_ENOUGH if results.empty?
+  rank(results)
+end
+
+def rank(results)
+  results.sort { |_a, b| b[:total] }
+  result_txt = ''.tap do |str|
+    str << "候補日ランキング\n"
+    (0..2).each do |n|
+      str << "#{n + 1}位: #{results[n][:day]}\n"
+    end
   end
 end
 
-results.sort { |_a, b| b[:total] }
-result_txt = ''.tap do |str|
-  str << "候補日ランキング\n"
-  (0..2).each do |n|
-    str << "#{n + 1}位: #{results[n][:day]}\n"
-  end
-end
-
-puts result_txt
-
-SendMessageToDiscord.new(result_txt).notify
+SendMessageToDiscord.new(result_text(visit_page(session, ARGV[0]))).notify
